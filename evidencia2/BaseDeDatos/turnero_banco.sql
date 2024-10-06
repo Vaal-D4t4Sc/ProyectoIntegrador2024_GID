8a45a814c8ca1bc7b51614899d65b80e9bffe054
Eglimar Ramirez <109255092+EglimarRamirez@users.noreply.github.com>
2024-10-06 13:03:44 -0300

    Update turnero_banco.sql

--- evidencia2/BaseDeDatos/turnero_banco.sql
+++ evidencia2/BaseDeDatos/turnero_banco.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `turnero_banco` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `turnero_banco`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: turnero_banco
